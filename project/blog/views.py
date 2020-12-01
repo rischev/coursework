c2c89ac27bc6676bcb67182dd9ffcb11c0911631
rischev <romanscherbakov.i@gmail.com>
2020-12-01 17:17:36 +0500

    progress

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog post 1',
-        'content': 'Sirst post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request) -> HttpResponse:
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
