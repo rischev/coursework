02d2dc544dc7f9e7bc092889a226841593f4c29c
rischev <romanscherbakov.i@gmail.com>
2020-12-09 23:36:28 +0500

    mmhaskell parse(no video)

--- project/blog/models.py
+++ project/blog/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from embed_video.fields import EmbedVideoField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = EmbedVideoField(blank=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+def deleteAll():
+    Post.objects.all().delete()
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,9 +1,13 @@
+
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+import requests
+import re
+from bs4 import BeautifulSoup
 
 def home(request) -> HttpResponse:
     context = {
@@ -67,3 +71,45 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
         # dobavit v otchet
+
+def mmhaskell() -> [Post]:
+    def find_title(string) -> str:
+        titleSoup = BeautifulSoup(string, 'html.parser')
+        links = titleSoup.find_all('a')
+        titleLink = links[2]
+        title = re.findall(re.compile(">(.*?)</a>"), str(titleLink))
+        return title[0]
+
+    def find_text_content(string, is_p) -> str:
+        if is_p:
+            return re.findall(re.compile('<p>(.*?)</p>'), string)[0]
+        else:
+            contentSoup = BeautifulSoup(string, 'html.parser')
+            return str(contentSoup.find_all("div", class_="sqs-block-content")[1])
+
+    def find_youtube_embed(string) -> str:
+        video = re.findall(re.compile("youtube.com/(.*?)\\&"), string)
+        if len(video) > 0:
+            return ("www.youtube.com/" + re.findall(re.compile("youtube.com/(.*?)\\&"), string)[0])
+        return None
+
+    html = requests.get('https://mmhaskell.com/blog').text
+    soup = BeautifulSoup(html, 'html.parser')
+    articles = soup.find_all('article')
+    posts = []
+    pool = Post.objects.all()
+    mmhaskellAuthor = User.objects.filter(username='mmhaskell').first()
+    for i in articles:
+        article_video = find_youtube_embed(str(i))
+        if article_video:
+            article_content = find_text_content(str(i), True)
+            post = Post(title=find_title(str(i)), content=article_content,
+                        video=article_video, author=mmhaskellAuthor)
+        else:
+            article_content = find_text_content(str(i), False)
+            post = Post(title=find_title(str(i)), content=article_content,
+                        author=mmhaskellAuthor)
+        if post not in pool:
+            post.save()
+        posts.append(post)
+    return posts
